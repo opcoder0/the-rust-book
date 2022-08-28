b6f5df2b656b5bb586cccbb0cf91abaaa4a62277
opcoder0 <110003254+opcoder0@users.noreply.github.com>
2022-08-28 16:38:01 +1000

    owership, borrowing, parameter passing

--- ownership_move_4/src/main.rs
+++ ownership_move_4/src/main.rs
@@ -1,8 +1,7 @@
 fn main() {
     let s1: String = String::from("hello");
     println!("s1 is {}", s1);
-    // ownership is now moved to s2.
-    // That way only one owner
+    // ownership is now moved to s2
     let s2 = s1;
     // cannot use s1 after the line above
     // compiler error - value borrowed/used after
@@ -10,6 +9,15 @@ fn main() {
     println!("s2 is {}", s2);
 
     scoped_ownership_move();
+
+    // ownership is transferred when passing parameters
+    let (s2, len) = word_length(s2);
+    println!("Word {s2} has {len} bytes");
+
+    let mut s2 = String::from("Hello ");
+    println!("before append: {s2}");
+    append_world(&mut s2);
+    println!("after  append: {s2}");
 }
 
 fn scoped_ownership_move() {
@@ -20,4 +28,13 @@ fn scoped_ownership_move() {
         println!("s2: {}", s2);
     }
     println!("done");
+}
+
+fn word_length(s: String) -> (String, usize) {
+    let len = s.len();
+    (s, len)
+}
+
+fn append_world(s: &mut String) {
+    s.push_str("world")
 }
