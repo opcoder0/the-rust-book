5533e388518dfc321288a3dbd0ec97f27b0a23f3
opcoder0 <110003254+opcoder0@users.noreply.github.com>
2022-09-06 16:35:22 +1000

    refactor vector code based on chapter topics

--- ch8/collections/README.md
+++ ch8/collections/README.md
@@ -1,3 +1,16 @@
 ## Collections
 
-Template based collections are defined in the standard library (`std::collections`); The data here is stored on the heap and hence the size of the data can be grown / shrunk at runtime. Some of the most common collections are `Vec` which allows you to store data contiguously and `String` which is a sequence of characters and `HashMap` a key-value pair accessed by hashing. There are others like `VecDequeue`, `LinkedList`, `BTreeMap`, `HashSet` etc.
+Template based collections are defined in the standard library (`std::collections`); The data here is stored on the heap and hence the size of the data can be grown / shrunk at runtime. Some of the most common collections are `Vec` available in `std::vec` which allows you to store data contiguously and `String` which is a sequence of characters and `HashMap` a key-value pair accessed by hashing. There are others like `VecDequeue`, `LinkedList`, `BTreeMap`, `HashSet` etc (in `std::collections`).
+
+## Vectors
+
+Vectors (`std::vec`) -
+
+- Is a generic container type
+- Store elements contiguously on the heap
+- All elements must be of the same type
+- When a vector is dropped all its elements are dropped as well
+
+_NOTE_: To store different element types in a vector use enum with associated values.
+
+
--- ch8/collections/vector/src/main.rs
+++ ch8/collections/vector/src/main.rs
@@ -1,82 +1,92 @@
+#[derive(Debug)]
+struct Element {
+    name: String,
+    value: i32,
+}
+
 fn main() {
     println!("simple initialization:");
     simple_initialization();
     println!();
 
-    println!("accessing vector element by reference / mutable reference");
-    referencing();
+    println!("accessing element from Vec<i32>");
+    get_zeroth_element();
+    println!();
+
+    println!("accessing element from Vec<Element>");
+    get_zeroth_struct_element();
+    println!();
+
+    println!("iterating over a vector and modifying elements");
+    iterate_vector();
+    println!();
+
+    println!("vector dropped");
+    vector_dropped();
     println!();
 }
 
+fn get_zeroth_element() {
+    let v = vec![1, 2, 3, 4, 5];
+    println!("vec: {:?}", v);
+    println!("getting to the zero-th element");
+    let zero = v[0];
+    println!("zero-th element {}", zero);
+}
+
+fn get_zeroth_struct_element() {
+    let mut v: Vec<Element> = Vec::new();
+    v.push(Element {
+        name: "zero-th".to_string(),
+        value: 0,
+    });
+    v.push(Element {
+        name: "first".to_string(),
+        value: 1,
+    });
+    v.push(Element {
+        name: "second".to_string(),
+        value: 2,
+    });
+    println!("vector<Element>: {:?}", v);
+    println!("getting first element");
+    let first: &Element = &v[1];
+    println!("first element from vector<Element>: {:?}", first);
+    println!("first.name: {}, first.value: {}", first.name, first.value);
+
+    // Accessing the first element directly like this
+    // let first = v[1];
+    // throws a compilation error saying a move is being attempted
+    // the type Element does not have a copy trait implemented.
+}
+
 fn simple_initialization() {
-    // initialize vector
-    println!("initialize vector using Vec::new()");
+    println!("Vec::new() - initialization");
     let mut v: Vec<i32> = Vec::new();
     v.push(1);
     v.push(2);
     v.push(3);
-    for i in v {
-        print!("{} ", i);
-    }
-    println!();
+    println!("v<i32>: {:?}", v);
 
-    // initialize vector with vec! macro
-    println!("initialize vector using vec! macro");
-    let mut v = vec![1, 2, 3, 5, 6];
-    println!("vector: (before insert) - {:?}", v);
-    v.insert(3, 4);
-    println!("vector: (after insert) - index(3) - {:?}", v);
-    for i in v {
-        print!("{} ", i);
-    }
-    println!();
+    println!("vec! - macro initialization");
+    let v = vec![1, 2, 3, 4, 5];
+    println!("vec!: {:?}", v);
 }
 
-struct Person {
-    name: String,
-    email: String,
+fn iterate_vector() {
+    let mut v = vec![1, 2, 3, 4, 5];
+    println!("Doubling vector: {:?}", v);
+    for i in &mut v {
+        *i *= 2;
+    }
+    println!("Output vector: {:?}", v);
 }
 
-fn referencing() {
-    // referencing element -- mutable, value
-    let mut people: Vec<Person> = Vec::new();
-    let names = ["sb", "pt", "cr", "sq"];
-    let emails = [
-        "sb@example.org",
-        "pt@example.org",
-        "cr@example.org",
-        "sq@example.org",
-    ];
-    let mut i: usize = 0;
-    while i < names.len() {
-        let p: Person = Person {
-            name: names[i].to_string(),
-            email: emails[i].to_string(),
-        };
-        people.push(p);
-        i += 1;
+fn vector_dropped() {
+    {
+        let v = vec![1, 2, 3, 4, 5];
+        println!("vec: {:?}", v);
     }
-    for p in &people {
-        println!("Name: {}, Email: ({})", p.name, p.email);
-    }
-
-    let pk: Person = Person {
-        name: "pk".to_string(),
-        email: "pk@example.org".to_string(),
-    };
-
-    println!("=== Adding one more (pk) ===");
-    people.push(pk);
-    for p in &people {
-        println!("Name: {}, Email: ({})", p.name, p.email);
-    }
-
-    let mut p4 = &mut people[4];
-    println!("=== changing email of {} ===", p4.name);
-    p4.email = "pk@devious.net".to_string();
-
-    println!("=== after email modification ===");
-    for p in &people {
-        println!("Name: {}, Email: ({})", p.name, p.email);
-    }
+    // vector v is not available here. The vector and all
+    // its elements are dropped
 }
