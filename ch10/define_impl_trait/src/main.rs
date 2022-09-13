cd3484fae7f894ecfc3551e0f7de64ba7b065cac
opcoder0 <110003254+opcoder0@users.noreply.github.com>
2022-09-13 13:07:51 +1000

    re-org into modules

--- ch10/define_impl_trait/src/main.rs
+++ ch10/define_impl_trait/src/main.rs
@@ -1,32 +1,6 @@
-pub trait Summary {
-    fn summarize(&self) -> String;
-}
+use aggregator::{NewsArticle, Summary, Tweet};
 
-pub struct NewsArticle {
-    pub headline: String,
-    pub location: String,
-    pub author: String,
-    pub content: String,
-}
-
-impl Summary for NewsArticle {
-    fn summarize(&self) -> String {
-        format!("{}, by {} ({})", self.headline, self.author, self.location)
-    }
-}
-
-pub struct Tweet {
-    pub username: String,
-    pub content: String,
-    pub retweet: bool,
-    pub reply: bool,
-}
-
-impl Summary for Tweet {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
-    }
-}
+mod aggregator;
 
 fn main() {
     let article: NewsArticle = NewsArticle {
