af9a785f58182275edd3ca3adfb120d3f553d576
opcoder0 <110003254+opcoder0@users.noreply.github.com>
2022-09-19 17:59:21 +1000

    Add fn to do case insensitive search

--- ch12/minigrep/src/lib.rs
+++ ch12/minigrep/src/lib.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
@@ -22,13 +23,14 @@ Pick three.";
 Rust:
 safe, fast, productive.
 Pick three.";
-        assert_eq!(vec!["Rust:"], search(query, content));
+        assert_eq!(vec!["Rust:"], search_case_insensitive(query, content));
     }
 }
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -38,7 +40,12 @@ impl Config {
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
@@ -47,7 +54,12 @@ impl Config {
 //
 pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(&config.file_path)?;
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+    for line in results {
         println!("{}", line);
     }
     Ok(())
@@ -57,6 +69,18 @@ pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
     for line in content.lines() {
         if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    let query = query.to_lowercase();
+
+    for line in content.lines() {
+        if line.to_lowercase().contains(&query) {
             results.push(line);
         }
     }
