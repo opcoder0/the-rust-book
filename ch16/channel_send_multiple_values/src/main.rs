a89f36c4e16d3e83f394aa81fbf2af64d20c2eef
opcoder0 <110003254+opcoder0@users.noreply.github.com>
2022-10-16 20:44:27 +1100

    add delay between sends

--- ch16/channel_send_multiple_values/src/main.rs
+++ ch16/channel_send_multiple_values/src/main.rs
@@ -1,5 +1,6 @@
 use std::sync::mpsc;
 use std::thread;
+use std::time::Duration;
 
 fn main() {
     let (tx, rx) = mpsc::channel();
@@ -8,10 +9,12 @@ fn main() {
         let msg = vec!["hi", "from", "the", "thread"];
         for s in msg {
             tx.send(s).unwrap();
+            thread::sleep(Duration::from_secs(1));
         }
     });
 
     for msg in rx {
         println!("received in main: {}", msg);
     }
+    println!("done: main end");
 }
